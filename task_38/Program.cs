b23fde38bafffe071063af685dc00f8a6da49708
Alex Lovy <Alex.lovy@gmail.com>
2023-05-26 00:06:02 +0300

    add change task 38

--- task_38/Program.cs
+++ task_38/Program.cs
@@ -2,58 +2,53 @@
 
 // [3,1; 7,2; 22,3; 2,4; 78,5] -> 76,1
 
-double[] FillArrayWithRandomNumber(int size)
-{
+// double [] arr = {3.1, 7.2, 22.3, 2.4, 78.5};
+
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine()); 
 
 double [] arr = new double[size];  
 
-Random rnd = new Random();
+
 
 for (int i = 0; i < arr.Length; i++)
+
   {
 
-  arr[i] = (double) rnd.Next(0,1000)/10;
+  Console.WriteLine($"Введите {i+1}-е число: ");
+  
+ arr[i] = Double.Parse(Console.ReadLine());
 
   }
 
-  return arr;
+void SelectionSort(double[] array)
 
-}
-
-System.Console.Write("Введите размер массива: ");
-
-int length = Convert.ToInt32(Console.ReadLine());
-
-double[] array = FillArrayWithRandomNumber(length);
-
-System.Console.WriteLine(string.Join("/ ", array));
-
-for (int i = 0; i< array.Length - 1; i++  )
-{  
+{
+for (int i = 0; i< array.Length - 1; i++)
+  {  
    int minPosition = i;
 
-    for (int j = i + 1; j < array.Length; j++)
+    for (int j = i + 1; j<array.Length; j++)
     
     {
-
-       if (array[j] < array[minPosition]) minPosition = j;
-
+        if (array[j] < array[minPosition]) minPosition = j;
     }
-  double temporary = array[i];
-
+    double temporary = array[i];
     array[i] = array[minPosition];
-
     array[minPosition] = temporary;
 
-    Console.WriteLine($"array[i] {array[i]}");
-    Console.WriteLine($"array[m] {array[minPosition]}");
-    Console.WriteLine($"temporar {temporary}");
-    Console.WriteLine(i);
-}
+  
+   }
 
-for (int i = 0; i< array.Length; i++  )
-{ 
-  System.Console.WriteLine(array[i]);
+  System.Console.Write($"Разница между максимальным и минимальным элементом массива равна: {array[array.Length - 1] - array[0]} ");
+  
 }
+  
 
-System.Console.Write($"Разница между максимальным и минимальным элементом массива равна: {array[array.Length - 1] - array[0]} ");
+SelectionSort(arr);
+
+    
+
+
+
